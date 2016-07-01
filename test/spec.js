2df2e3bd148338b54fb909cd708d95ee4c7790a3
Jinsoo Cha <jinscha@indiana.edu>
2016-06-30 17:05:31 -0700

    fix travis

--- test/spec.js
+++ test/spec.js
@@ -1,11 +1,11 @@
 require('./setup');
-const appUrl = `${process.env.PROTOCOL}${process.env.HOST}:${process.env.PORT}`;
+// const appUrl = `${process.env.PROTOCOL}${process.env.HOST}:${process.env.PORT}`;
 const request = require('supertest');
 
 describe('Gobble recommendation', () => {
   describe('making a recommendation', () => {
     it('should return status code 201 and an object', (done) => {
-      request(appUrl)
+      request('http://localhost:3000')
         .post('/api/getRecommendation')
         .send({ UPC: 1 })
         .expect(201, 'object')
